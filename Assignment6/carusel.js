492b706c83fbb5c20dd28c7d12b3f86724defffa
gentlemankz <shynggys.zhakenuly@gmail.com>
2024-10-25 00:02:02 +0500

    upgrade

--- Assignment6/carusel.js
+++ Assignment6/carusel.js
@@ -1,26 +1,24 @@
-
 const carousel = document.querySelector('.carousel-list');
 const leftArrow = document.querySelector('.left-arrow');
 const rightArrow = document.querySelector('.right-arrow');
 
-
 let currentIndex = 0;
-const itemWidth = 140; 
+const itemWidth = 140;
+const visibleItems = Math.floor(document.querySelector('.carousel').clientWidth / itemWidth);
+const maxIndex = carousel.children.length - visibleItems;
+
+const updateCarouselPosition = () => {
+  carousel.style.transform = `translateX(${-currentIndex * itemWidth}px)`;
+};
 
 leftArrow.addEventListener('click', () => {
-  if (currentIndex > 0) {
-    currentIndex--;
-    updateCarouselPosition();
-  }
+  currentIndex = currentIndex > 0 ? currentIndex - 1 : maxIndex;
+  updateCarouselPosition();
 });
 
 rightArrow.addEventListener('click', () => {
-  if (currentIndex < carousel.children.length - Math.floor(carousel.clientWidth / itemWidth)) {
-    currentIndex++;
-    updateCarouselPosition();
-  }
+  currentIndex = currentIndex < maxIndex ? currentIndex + 1 : 0;
+  updateCarouselPosition();
 });
-function updateCarouselPosition() {
-  const offset = -currentIndex * itemWidth;
-  carousel.style.transform = `translateX(${offset}px)`;
-}
+
+updateCarouselPosition();
